7216cec90262f554119950ae8ab9bf05241fb90b
KANG SEUNG HOON <deforeturn@gmail.com>
2021-09-18 13:08:25 +0900

    특정 범위에 대한 소수 구하는 함수

---
+++ Python/prime_getter.py
@@ -0,0 +1,26 @@
+def prime_getter(range_of):
+    prime_numbers = [2, 3, 5, 7]
+    if range_of <= prime_numbers[-1]:
+        sub_result = []
+        for i in range(4):
+            if prime_numbers[i] <= range_of:
+                sub_result.append(prime_numbers[i])
+            else:
+                return set(sub_result)
+    while True:
+        start_num = prime_numbers[-1]
+        push_primes = []
+        end_num = 0
+        for num in range(start_num + 1, (start_num * start_num) + 1):
+            if num > range_of:
+                end_num = num
+                break
+            push_primes.append(num)
+            for prime in prime_numbers:
+                if num%prime == 0:
+                    push_primes.pop(-1)
+                    break
+        prime_numbers.extend(push_primes)
+        if end_num >= range_of:
+            break
+    return set(prime_numbers)
